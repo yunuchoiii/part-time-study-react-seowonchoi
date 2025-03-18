f1995abdcf923ade843e085fbf9a589bedd7a28d
Seowon Choi <chltjdnjs529@gmail.com>
2025-03-18 14:15:03 +0900

    테스트 코드 관련 라이브러리 설치 및 설정 파일 생성

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  
+};
+
+export default config;
